9b04704acd6840ba70d5cdef79cf17f69e7965c3
rlwjd31 <rlwjd31@naver.com>
2025-11-12 18:15:44 +0900

    🚀[Feat]: Install jotai and wrap with jotai provider to root

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
+import JotaiProvider from "@/app/providers/JotaiProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <JotaiProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </JotaiProvider>
       </body>
     </html>
   );
