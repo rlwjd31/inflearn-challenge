4d01c01d05a57f8b93b15b373c5a2d6079781b61
rlwjd31 <rlwjd31@naver.com>
2025-11-12 18:17:29 +0900

    ♻️[Refactor]: Update each provider use combined Providers
    index.tsx를 진입점으로 하여 provider들을 관리

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
 import JotaiProvider from "@/app/providers/JotaiProvider";
+import Providers from "@/app/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <JotaiProvider>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
-        </JotaiProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
