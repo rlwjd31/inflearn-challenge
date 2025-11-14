c0b35579269409aef57c882636e9d861908cd52d
rlwjd31 <rlwjd31@naver.com>
2025-11-14 15:52:46 +0900

    🚀[Feat]: Implement compare plain password with hashed password by bcryptjs
    로그인 구현 시 평문의 비밀번호와 해싱된 비밀번호의 값을 비교하기 위한 유틸 함수 구현

--- frontend/shared/lib/auth-util.ts
+++ frontend/shared/lib/auth-util.ts
@@ -1,4 +1,4 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
 // * 오늘날 가장 많이 사용되는 salt round 값은 10 ~ 12
 // * 값이 커질수록 해시 계산이 느려져 보안성은 커지지면 응답 지연이 발생
@@ -9,3 +9,8 @@ export const getHashedPassword = async (
   password: string,
   saltRound: number = 10
 ) => hash(password, saltRound);
+
+export const comparePassword = async (
+  plainPassword: string,
+  hashPassword: string
+) => compare(plainPassword, hashPassword);
