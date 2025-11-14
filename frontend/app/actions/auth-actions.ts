8d8dd5de8edf177584e5c742094c20f6e5c8a60d
rlwjd31 <rlwjd31@naver.com>
2025-11-14 14:39:06 +0900

    🚀[Feat]: Implement user sign-up action with credential(email, pw) validation
    - 존재하는 유저인지 검사
    - message, status를 가지는 객체를 response로 하여 front단에서 적절히 처리할 수 있도록 구현

---
+++ frontend/app/actions/auth-actions.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { prisma } from "@/prisma";
+import { getHashedPassword } from "@/shared/lib/auth-util";
+
+// TODO: front에서 backend의 repository의 작업을 수행(user create)를 하고 있으므로 추후 backend에서 api를 만들고 호출하는 식으로 변경하여 domain을 분리하여 관심사를 명확히 하기.
+export const signUp = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}): Promise<{
+  status: number;
+  message: string;
+}> => {
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      // ? 409 conflict status code
+      return { status: 409, message: "이미 존재하는 사용자입니다" };
+    }
+
+    // * 존재하는 유저가 아닐 때 새로운 user 생성
+    const hashedPassword = await getHashedPassword(password);
+    await prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
+    return { status: 201, message: "회원가입에 성공했습니다." };
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "";
+    console.error(errorMessage);
+
+    return { status: 500, message: "회원가입에 실패했습니다" };
+  }
+};
