0cbd304d38c026c9e68f00dd0a8de46e1d63530a
rlwjd31 <rlwjd31@naver.com>
2025-11-15 01:26:31 +0900

    🚀[Feat]: Apply swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // * swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('clone inflearn API 문서')
+    .setDescription('api 상세 문서')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
