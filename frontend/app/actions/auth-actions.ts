83256e4e8003bd9444f3ca54cbaf1aa91d81ef89
rlwjd31 <rlwjd31@naver.com>
2025-11-14 16:55:21 +0900

    🚀[Feat]: Implement sign-in action using AuthJS for email, pw authentication

--- frontend/app/actions/auth-actions.ts
+++ frontend/app/actions/auth-actions.ts
@@ -1,7 +1,20 @@
 "use server";
 
+import { signIn as signInWithAuthJS } from "@/auth";
 import { prisma } from "@/prisma";
 import { getHashedPassword } from "@/shared/lib/auth-util";
+
+// * @/auth에서 불러온 signIn(해당 코드에서는 signInWithAuthJS)함수를 호출하면 auth.ts에 작성한
+// * authorize함수가 호출된다.
+export const signIn = async (formData: FormData, redirectURL: string = "/") => {
+  const result = await signInWithAuthJS("credentials", {
+    email: formData.get("email"),
+    password: formData.get("password"),
+    redirectTo: redirectURL,
+  });
+
+  return result;
+};
 
 // TODO: front에서 backend의 repository의 작업을 수행(user create)를 하고 있으므로 추후 backend에서 api를 만들고 호출하는 식으로 변경하여 domain을 분리하여 관심사를 명확히 하기.
 export const signUp = async ({
