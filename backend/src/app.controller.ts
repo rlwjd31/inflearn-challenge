ff27db3d8574a46429775b4ac60b427d3bf2cb5d
rlwjd31 <rlwjd31@naver.com>
2025-11-15 17:22:30 +0900

    🚀[Feat]: Add user test endpoint with JWT guard
    임시적으로 test를 진행할 것이므로 app controller에서 진행함.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JWTAccessTokenGuard } from 'src/ auth/guards/jwt-access-token.guard';
+import { Request as ExpressRequest } from 'express';
+
+type JWTPayload = {
+  sub: string;
+  username: string;
+};
 
 @Controller()
 export class AppController {
@@ -8,5 +15,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // TODO: 임시 test code이므로 추후 auth controller로 이전 해야 함.
+  @Get('user-test')
+  @UseGuards(JWTAccessTokenGuard) // @UseGuards(new JWTAccessTokenGuard())와 같이 되는 것이므로 해당 전략을 사용하기 위해서 module에서 import를 따로 할 필요가 없다.
+  testUser(@Request() req: ExpressRequest & { user: JWTPayload }): JWTPayload {
+    console.log(req);
+
+    return req.user;
   }
 }
