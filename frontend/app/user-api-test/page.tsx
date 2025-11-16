ef665f8f424abac73d7f3657ad3873c3cb721d22
rlwjd31 <rlwjd31@naver.com>
2025-11-16 23:05:42 +0900

    🚀[Feat]: Test fetch well from server component

--- frontend/app/user-api-test/page.tsx
+++ frontend/app/user-api-test/page.tsx
@@ -1,8 +1,11 @@
 import ClientTestUser from "@/app/user-api-test/client-test";
+import ServerTestUser from "@/app/user-api-test/server-test";
 
 export default function UserTestPage() {
   return (
     <div>
+      <ServerTestUser />
+      <div className="h-12" />
       <ClientTestUser />
     </div>
   );
