7e88e178e95d6a7ecd6f65e0144edd50c641334d
rlwjd31 <rlwjd31@naver.com>
2025-11-19 21:41:09 +0900

    🚀[Feat]: Eage connect prisma when nestjs app startup
    eager connection을 통해 nestjs app이 구동 시 prisma를 통해 db연결 즉시 설정
    
    - eager connection
    - nestjs 종료시 안전하게 db와 연결 종료(onModuleDestroy)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/ auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({})],
+  imports: [AuthModule, ConfigModule.forRoot({}), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
