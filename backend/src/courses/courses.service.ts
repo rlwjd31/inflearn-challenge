c29ea67ec85717ca638ed6dbcdc1b582d5807ec0
rlwjd31 <rlwjd31@naver.com>
2025-11-20 18:48:58 +0900

    🚀[Feat]: Implement course service with prisma
    강좌 정보에 대해 db와의 작업을 수행하기 위해 prisma를 활용하여 service를 작성

---
+++ backend/src/courses/courses.service.ts
@@ -0,0 +1,104 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Course, Prisma } from '@prisma/client';
+import { CreateCourseDto } from 'src/courses/dto/create-course.dto';
+import { UpdateCourseDto } from 'src/courses/dto/update-course.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CoursesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(
+    userId: string,
+    createCourseDto: CreateCourseDto,
+  ): Promise<Course> {
+    const { categoryIds, ...otherData } = createCourseDto;
+
+    return this.prisma.course.create({
+      data: {
+        ...otherData,
+        categories: {
+          connect: (categoryIds ?? []).map((id) => ({ id })),
+        },
+        instructorId: userId,
+      },
+    });
+  }
+
+  async findAll(params: Prisma.CourseFindManyArgs): Promise<Course[]> {
+    return this.prisma.course.findMany({ ...params });
+  }
+
+  async findOne(
+    id: string,
+    include: Prisma.CourseFindUniqueArgs['include'],
+  ): Promise<Course | null> {
+    return this.prisma.course.findUnique({
+      where: { id },
+      include,
+    });
+  }
+
+  async update(
+    id: string,
+    userId: string,
+    updateCourseDto: UpdateCourseDto,
+  ): Promise<Course> {
+    const course = await this.prisma.course.findUnique({
+      where: { id },
+    });
+
+    if (!course) {
+      throw new NotFoundException(`ID: ${id} 강좌를 찾을 수 없습니다.`);
+    }
+
+    if (course.instructorId !== userId) {
+      throw new UnauthorizedException('강의의 소유자만 수정할 수 있습니다.');
+    }
+
+    return this.prisma.course.update({
+      where: { id },
+      data: updateCourseDto,
+    });
+  }
+
+  async delete(id: string, userId: string) {
+    const course = await this.prisma.course.findUnique({
+      where: { id },
+    });
+
+    if (!course) {
+      throw new NotFoundException(`ID: ${id} 강좌를 찾을 수 없습니다.`);
+    }
+
+    if (course.instructorId !== userId) {
+      throw new UnauthorizedException('강의의 소유자만 삭제할 수 있습니다.');
+    }
+
+    return this.prisma.course.delete({
+      where: { id },
+    });
+  }
+
+  // async findAll(params: {
+  //   skip?: number;
+  //   take?: number;
+  //   cursor: Prisma.CourseWhereUniqueInput;
+  //   where?: Prisma.CourseWhereInput;
+  //   orderBy?: Prisma.CourseOrderByWithRelationInput
+  // }): Promise<Course[]> {
+
+  //   const { skip, take, cursor, where, orderBy } = params;
+  //   return this.prisma.course.findMany({
+  //     skip,
+  //     take,
+  //     cursor,
+  //     where,
+  //     orderBy,
+  //   });
+  // }
+}
