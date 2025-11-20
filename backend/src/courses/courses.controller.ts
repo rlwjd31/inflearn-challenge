b482f76c652ca392031daf12775080bdc45fb3e3
rlwjd31 <rlwjd31@naver.com>
2025-11-20 18:50:09 +0900

    🚀[Feat]: Connect service and request endpoint
    - swagger를 통해 param, query string, description 작성
    - useGuard를 통해 req.user를 주입하고 access-token을 endpoint도달 전에 검증
    - 특정 http method는 권한이 있는 사용자(강좌를 소유한 유저)에 대해서만 수행할 수 있도록 인가처리

---
+++ backend/src/courses/courses.controller.ts
@@ -0,0 +1,103 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { JWTAccessTokenGuard } from 'src/ auth/guards/jwt-access-token.guard';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import type { Request as ExpressRequest } from 'express';
+import { CreateCourseDto } from 'src/courses/dto/create-course.dto';
+import { Prisma } from '@prisma/client';
+import { UpdateCourseDto } from 'src/courses/dto/update-course.dto';
+
+@ApiTags('courses')
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Post()
+  @UseGuards(JWTAccessTokenGuard)
+  @ApiBearerAuth('access-token')
+  create(
+    @Request() req: ExpressRequest,
+    @Body() createCourseDto: CreateCourseDto,
+  ) {
+    return this.coursesService.create(req.user?.sub as string, createCourseDto);
+  }
+
+  @Get()
+  @ApiQuery({ name: 'title', required: false })
+  @ApiQuery({ name: 'level', required: false })
+  @ApiQuery({ name: 'categoryId', required: false })
+  @ApiQuery({ name: 'skip', required: false })
+  @ApiQuery({ name: 'take', required: false })
+  findAll(
+    @Query('title') title?: string,
+    @Query('level') level?: string,
+    @Query('categoryId') categoryId?: string,
+    @Query('skip') skip?: number,
+    @Query('take') take?: number,
+  ) {
+    return this.coursesService.findAll({
+      where: {
+        title: title ? { contains: title, mode: 'insensitive' } : undefined,
+        categories: categoryId ? { some: { id: categoryId } } : undefined,
+        level,
+      },
+      skip: skip ? +skip : undefined,
+      take: take ? +take : undefined,
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  @Get(':id')
+  @ApiQuery({
+    name: 'include',
+    required: false,
+    description: 'sections, lectures, courseReviews 등 포함한 관계 지정',
+  })
+  findOne(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Query('include') include?: string,
+  ) {
+    const includeArray = include ? include.split(',') : [];
+    const includeObject: Prisma.CourseFindUniqueArgs['include'] =
+      Object.fromEntries(includeArray.map((v) => [v, true]));
+
+    return this.coursesService.findOne(id, includeObject);
+  }
+
+  @Patch(':id')
+  @UseGuards(JWTAccessTokenGuard)
+  @ApiBearerAuth('access-token')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Request() req: ExpressRequest,
+    @Body() updateCourseDto: UpdateCourseDto,
+  ) {
+    return this.coursesService.update(
+      id,
+      req.user?.sub as string,
+      updateCourseDto,
+    );
+  }
+
+  @Delete(':id')
+  @ApiBearerAuth('access-token')
+  @UseGuards(JWTAccessTokenGuard)
+  delete(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Request() req: ExpressRequest,
+  ) {
+    return this.coursesService.delete(id, req.user?.sub as string);
+  }
+}
