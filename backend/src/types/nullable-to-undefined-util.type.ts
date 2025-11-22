d81aaacbc25f1e979c8e5e84d4f6bf6ef87b2d10
rlwjd31 <rlwjd31@naver.com>
2025-11-23 00:06:51 +0900

    ♻️[Refactor]: Apply optional type to null type
    title: string | undefined는 통과되고 title?: string은 허용하지 않아 optional이 적용될 수 있도록 util type수정

--- backend/src/types/nullable-to-undefined-util.type.ts
+++ backend/src/types/nullable-to-undefined-util.type.ts
@@ -25,5 +25,15 @@ export type NullToUndefined<T> = null extends T
  *
  */
 export type NullableToUndefined<T> = {
-  [K in keyof T]: NullToUndefined<T[K]>;
+  // null이 포함된 필드는 optional로
+  [K in keyof T as null extends T[K] ? K : never]?: Exclude<T[K], null>;
+} & {
+  // null이 없는 필드는 그대로
+  [K in keyof T as null extends T[K] ? never : K]: T[K];
 };
+
+// * as는 `타입 단언`과 `key remapping`을 수행한다.
+// * key에서 사용될 때는 키를 "변환"하는 역할을 한다.
+// * 예시: K in keyof T as (null extends T[K] ? K : never)
+// *      => key의 value의 type이 null을 포함한다면 key를 그대로 유지,
+// *       null을 포함하지 않는다면 never로 변환 -> 이때는 key가 제거된다.
