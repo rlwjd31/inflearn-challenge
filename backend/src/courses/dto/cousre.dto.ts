fa8cdaeac47be24edbd02ac8efc4563b411a7a67
rlwjd31 <rlwjd31@naver.com>
2025-11-22 21:14:39 +0900

    🚀[Feat]: Create common course dto fields by implementing prisma course model
    - prisma의 schema model의 정의가 바뀔 수 있으므로 해당 model을 구현(implements)
    - swagger 명세 작성
    - 해당 courseDTO는 공통 field를 모은 DTO를 이를 상속 또는 구현체로 사용되므로 class-validator를 통한 검증은 작성 ❌

---
+++ backend/src/courses/dto/cousre.dto.ts
@@ -0,0 +1,180 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Course,
+  CourseCategory,
+  CourseEnrollment,
+  CourseQuestion,
+  CourseReview,
+  Lecture,
+  Section,
+} from '@prisma/client';
+
+export enum CourseLevel {
+  BEGINNER = 'BEGINNER',
+  INTERMEDIATE = 'INTERMEDIATE',
+  ADVANCED = 'ADVANCED',
+}
+
+export enum CourseStatus {
+  DRAFT = 'DRAFT',
+  PUBLISHED = 'PUBLISHED',
+  ARCHIVED = 'ARCHIVED',
+}
+
+export class CourseDTO implements Course {
+  @ApiProperty({
+    description: '강좌 고유 식별 ID',
+    type: String,
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    readOnly: true, // ✅ 서버에서 자동 생성하는 필드는 readOnly를 넣어주어야 하낟.
+  })
+  id: string;
+
+  @ApiProperty({
+    description: '강좌 슬러그(URL에 사용됨)',
+    type: String,
+    example: 'course-slug',
+    required: true,
+  })
+  slug: string;
+
+  @ApiProperty({
+    description: '강좌 제목',
+    type: String,
+    example: 'NextJS 15 뿌수기',
+    required: true,
+  })
+  title: string;
+
+  @ApiProperty({
+    description: '강좌 1~2줄 짧은 설명',
+    type: String,
+    required: false,
+    nullable: true,
+  })
+  shortDescription: string | null;
+
+  @ApiProperty({
+    description: '강좌 상세페이지 설명',
+    type: String,
+    required: false,
+    nullable: true,
+  })
+  description: string | null;
+
+  @ApiProperty({
+    description: '강좌 썸네일 URL',
+    type: String,
+    required: false,
+    nullable: true,
+  })
+  thumbnailUrl: string | null;
+
+  @ApiProperty({
+    description: '강좌 가격(만원 단위)',
+    type: Number,
+    example: 10000,
+    required: true,
+  })
+  price: number;
+
+  @ApiProperty({
+    description: '강좌 할인 가격',
+    type: Number,
+    required: false,
+    nullable: true,
+  })
+  discountPrice: number | null;
+
+  @ApiProperty({
+    description: '강좌 난이도. ',
+    enum: CourseLevel,
+    example: CourseLevel, // 'beginner', 'intermediate', 'advanced'와 같이 string 형태로 작성을 해야 하나...?
+    default: CourseLevel.BEGINNER,
+  })
+  level: CourseLevel;
+
+  @ApiProperty({
+    description: '강좌 상태. e.g) DRAFT, PUBLISHED, ARCHIVED',
+    enum: CourseStatus,
+    example: CourseStatus,
+    default: CourseStatus.DRAFT,
+  })
+  status: CourseStatus;
+
+  @ApiProperty({
+    description: '강좌 생성 시간',
+    type: Date,
+    default: new Date(),
+    readOnly: true,
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    description: '강좌가 마지막으로 업데이트 된 시간',
+    type: Date,
+    default: new Date(),
+    readOnly: true,
+  })
+  updatedAt: Date;
+
+  // * FK
+  @ApiProperty({
+    description: '강좌 강사 ID',
+    type: String,
+    example: '550e8400-e29b-41d4-a716-446655440000',
+    required: true,
+  })
+  instructorId: string;
+
+  // * relations
+  @ApiProperty({
+    isArray: true,
+    // TODO: have to convert Object type to like () => SectionDTO after defined Section
+    // TODO: same as lectures, categories, enrollments, reviews, questions
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  sections: Section[] | null;
+
+  @ApiProperty({
+    isArray: true,
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  lectures: Lecture[] | null;
+
+  @ApiProperty({
+    isArray: true,
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  categories: CourseCategory[] | null;
+
+  @ApiProperty({
+    isArray: true,
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  enrollments: CourseEnrollment[] | null;
+
+  @ApiProperty({
+    isArray: true,
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  reviews: CourseReview[] | null;
+
+  @ApiProperty({
+    isArray: true,
+    type: Object,
+    required: false,
+    nullable: true,
+  })
+  questions: CourseQuestion[] | null;
+}
