325e5abd7f70771b781a842462d58c3195ee71d7
rlwjd31 <rlwjd31@naver.com>
2025-11-30 23:58:46 +0900

    🚀[Feat]: Implement create course api
    - openapi-ts의 error 형식을 parsing하여 error처리를 일관화함
    - ApiError type정의
    - 추후 재사용을 고려하여 throwApiError 구현

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,8 +2,18 @@
 
 import {
   categoriesControllerFindAll,
+  coursesControllerCreate,
   coursesControllerFindAll,
 } from "@/generated/openapi-client";
+
+type ApiError = {
+  statusCode: number;
+  message: string;
+};
+
+const throwApiError = (error: ApiError) => {
+  throw new Error(`${error.statusCode}: ${error.message}`);
+};
 
 export const getAllCategories = async () => {
   const { data, error } = await categoriesControllerFindAll();
@@ -18,4 +28,20 @@ export const getAllInstructorCourses = async () => {
   const { data, error } = await coursesControllerFindAll();
 
   return { data, error };
+};
+
+export const createCourse = async (title: string) => {
+  const { data, error } = await coursesControllerCreate({
+    body: {
+      title,
+    },
+  });
+
+  if (error) {
+    throwApiError(error as ApiError);
+  }
+
+  return {
+    data,
+  };
 };
