588326d705c03d9212e7cd2e5bfb9920d3e20ee6
rlwjd31 <rlwjd31@naver.com>
2025-12-01 00:05:10 +0900

    🚀[Feat]: Create course page implementation
    - 강의 생성 오류 시 toast
    - 강의 생성 성공 시 navigation

---
+++ frontend/app/create_courses/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import * as api from "@/lib/api";
+
+export default function CreateCoursePage() {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+
+  const createCourseMutation = useMutation({
+    mutationFn: () => api.createCourse(title),
+    onSuccess: ({ data }) => {
+      if (data) {
+        router.push(`/course/${data.id}/edit/course_info`);
+      }
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <div className="w-full max-w-xl mx-auto h-[90vh] flex flex-col items-center justify-center gap-4">
+      <h2 className="text-xl text-center font-bold">
+        제목을 입력해주세요!
+        <br />
+        너무 고민하지마세요. 제목은 언제든 수정 가능해요 :)
+      </h2>
+      <Input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="제목을 입력해주세요."
+        className="bg-[#F6F6F6] py-6 rounded-md"
+      />
+      <div className="space-x-2">
+        <Button variant="outline" className="px-8 py-6 text-md font-bold">
+          이전
+        </Button>
+        <Button
+          onClick={() => createCourseMutation.mutate()}
+          variant="default"
+          className="px-8 py-6 text-md font-bold"
+        >
+          만들기
+        </Button>
+      </div>
+    </div>
+  );
+}
