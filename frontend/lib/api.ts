14855ce79dbee21ddbbb87a6b3b9d177c3f647f2
rlwjd31 <rlwjd31@naver.com>
2025-12-02 04:56:40 +0900

    🚀[Feat]: Implement api function when need enroll, edit section page
    - create section 구현
    - updatesectiontitle 구현
      Related to: 55dfdb8
    - title기반 sectionupdate api함수 구현

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -6,6 +6,9 @@ import {
   coursesControllerFindAll,
   coursesControllerFindOne,
   coursesControllerUpdate,
+  sectionsControllerCreate,
+  sectionsControllerDelete,
+  sectionsControllerUpdate,
   UpdateCourseDto,
 } from "@/generated/openapi-client";
 
@@ -49,9 +52,12 @@ export const createCourse = async (title: string) => {
   };
 };
 
-export const getCourseById = async (id: string) => {
+export const getCourseById = async (id: string, include?: string) => {
   const { data, error } = await coursesControllerFindOne({
     path: { id },
+    query: {
+      include: include ?? "sections,lectures",
+    },
   });
 
   if (error) {
@@ -73,3 +79,44 @@ export const updateCourse = async (id: string, data: UpdateCourseDto) => {
 
   return { data: course };
 };
+
+// * ==================== CRUD Operation of section apis ====================
+export const createSection = async (courseId: string, title: string) => {
+  const { data: section, error } = await sectionsControllerCreate({
+    path: { courseId },
+    body: { title },
+  });
+
+  if (error) {
+    throwApiError(error as ApiError);
+  }
+
+  return { data: section };
+};
+
+export const updateSectionTitle = async (sectionId: string, title: string) => {
+  const { data: section, error } = await sectionsControllerUpdate({
+    path: { sectionId },
+    body: { title },
+  });
+
+  if (error) {
+    throwApiError(error as ApiError);
+  }
+
+  return { data: section };
+};
+
+export const deleteSection = async (sectionId: string) => {
+  const { data: section, error } = await sectionsControllerDelete({
+    path: { sectionId },
+  });
+
+  if (error) {
+    throwApiError(error as ApiError);
+  }
+
+  return { data: section };
+};
+
+
