24eec3246df96562ee2a0faa158732378b8f427a
rlwjd31 <rlwjd31@naver.com>
2025-12-13 14:18:44 +0900

    🚀[Feat]: Add TypeScript definitions for process environment

---
+++ backend/src/config/env.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+      AUTH_SECRET: string;
+      DEVELOPMENT_API_URL: string;
+      PRODUCTION_API_URL: string;
+      AWS_REGION: string;
+      AWS_ACCESS_KEY_ID: string;
+      AWS_SECRET_ACCESS_KEY: string;
+      AWS_CLOUDFRONT_DOMAIN: string;
+    }
+  }
+}
+
+export {};
