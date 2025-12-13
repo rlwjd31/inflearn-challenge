46445e144260ce0e5792cdcee857fb4c32195b53
rlwjd31 <rlwjd31@naver.com>
2025-12-13 23:13:29 +0900

    🚀[Feat]: Customized CKEditor to use markdown
    - 관련 tool을 plugin으로 정의
    - toolbar를 통해 editor의 format 정의

---
+++ frontend/components/CustomCKEditor.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Heading, // h1, h2, h3
+  Essentials, // do, undo
+  Paragraph, // paragraph
+  Bold,
+  Link,
+  List, // bulletedList, numberedList
+  Indent, // indent, outdent
+  BlockQuote,
+  Image, // ImageUpload랑 같이 사용해야 함
+  ImageUpload,
+  Table,
+  Italic,
+  EventInfo,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+
+import { useEffect, useRef, useState } from "react";
+
+type CustomCKEditorProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+// TODO: auto saving 기능 추가하기 -> https://ckeditor.com/docs/ckeditor5/latest/features/autosave.html
+export default function CustomCKEditor({
+  value,
+  onChange,
+}: CustomCKEditorProps) {
+  const editorRef = useRef<ClassicEditor>(null);
+  const editorToolbarRef = useRef<HTMLDivElement>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  const handleEditorReady = (classicEditor: ClassicEditor) => {
+    editorRef.current = classicEditor;
+  };
+
+  const handleOnChange = (event: EventInfo, editor: ClassicEditor) => {
+    const data = editor.getData();
+    // onChange(data);
+  };
+
+  return (
+    <div className="ck-editor-container">
+      <div ref={editorToolbarRef} className="min-h-[300px] border p-4 rounded">
+        {!isMounted && <p>Loading editor...</p>}
+        {isMounted && (
+          <CKEditor
+            editor={ClassicEditor}
+            onReady={handleEditorReady}
+            onChange={handleOnChange}
+            data={value}
+            config={{
+              licenseKey: "GPL",
+              plugins: [
+                Heading,
+                Essentials,
+                Paragraph,
+                Bold,
+                Italic,
+                Link,
+                List,
+                Indent,
+                BlockQuote,
+                Table,
+                Image,
+                ImageUpload,
+              ],
+              toolbar: [
+                "heading",
+                "|",
+                "bold",
+                "italic",
+                "link",
+                "bulletedList",
+                "numberedList",
+                "|",
+                "outdent",
+                "indent",
+                "|",
+                "imageUpload",
+                "blockQuote",
+                "insertTable",
+                "undo",
+                "redo",
+              ],
+              initialData: "<p>Hello from CKEditor 5 in React!</p>",
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
