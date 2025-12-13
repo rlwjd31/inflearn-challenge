6b35fb7f1778dd7e88341e85b4edcb5a0738a75a
rlwjd31 <rlwjd31@naver.com>
2025-12-13 15:50:15 +0900

    🚀[Feat]: Implement media upload service to S3
    - aws-sdk를 이용한 파일 업로드 service 구현
    - s3직접 접근은 막아놔 cloudfront domain을 통해서 접근
    - 객체 접근 url은 getMediaUrl을 통해서 생성

--- backend/src/media/media.service.ts
+++ backend/src/media/media.service.ts
@@ -1,6 +1,6 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-
+import { v4 as uuid } from 'uuid';
 @Injectable()
 export class MediaService {
   private s3Client: S3Client;
@@ -15,5 +15,42 @@ export class MediaService {
       },
     });
     this.cloudFrontDomain = process.env.AWS_CLOUDFRONT_DOMAIN;
+  }
+
+  async uploadMedia(file: Express.Multer.File, userId: string) {
+    const fileExtension = file.originalname.split('.').pop();
+    const key = `media/${userId}/${uuid()}.${fileExtension}`;
+
+    const response = await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.AWS_MEDIA_S3_BUCKET_NAME,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      }),
+    );
+
+    // video storage field
+    return {
+      storageType: 's3',
+      s3: {
+        bucket: process.env.AWS_MEDIA_S3_BUCKET_NAME,
+        key,
+        size: response.Size,
+        region: process.env.AWS_REGION,
+        // endpoint: process.env.AWS_MEDIA_S3_ENDPOINT, 👉🏻 s3에 직접 접근할 때는 필요하지만 cloudfront를 통해서 접근할 때는 필요가 없다.
+        metadata: {
+          uploadedAt: new Date().toISOString(),
+          contentType: file.mimetype,
+        },
+        cloudFront: {
+          url: this.getMediaUrl(key),
+        },
+      },
+    };
+  }
+
+  getMediaUrl(key: string) {
+    return `https://${this.cloudFrontDomain}/${key}`;
   }
 }
