48602c13e594de5c6a37a877ceda141a7bd058ba
rlwjd31 <rlwjd31@naver.com>
2025-12-17 23:08:33 +0900

    🚀[Feat]: Implement edit course cover image UI & logic
    - change layout ui props course title to course
    - 썸네일 이미지 onDrop시 update course thumbnail api 실행

--- frontend/app/course/[id]/edit/layout.tsx
+++ frontend/app/course/[id]/edit/layout.tsx
@@ -30,7 +30,7 @@ export default async function EditCourseLayout({
 
   return (
     <div className="w-full h-full bg-[#F1F3F5]">
-      <EditCourseHeader title={course.title} />
+      <EditCourseHeader course={course} />
       <div className="p-12 flex gap-12 min-h-screen max-w-5xl">
         <EditCourseSidebar />
         <div className="flex-1">{children}</div>
