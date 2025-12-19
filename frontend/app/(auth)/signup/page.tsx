b59779de140e182680922aa77212f221de33328a
rlwjd31 <rlwjd31@naver.com>
2025-12-19 23:29:43 +0900

    🚀[Feat]: Add metadata to every pages

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "회원가입 - fakeInflearn",
   description: "Fake inflearn(인프런) 회원가입 페이지입니다.",
 };
+
 export default function SignUpPage() {
   return <SignUpUI />;
 }
