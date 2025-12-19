82e9d08072fc894f3d354cdee44a5fa0d43d38e0
rlwjd31 <rlwjd31@naver.com>
2025-12-19 23:20:31 +0900

    🚀[Feat]: Add metadata to SignIn page for improved SEO
    - title metatdata추가
    - description  metatdata추가

--- frontend/app/(auth)/signin/page.tsx
+++ frontend/app/(auth)/signin/page.tsx
@@ -1,4 +1,10 @@
 import SignInUI from "@/app/(auth)/signin/SignInUI";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "로그인 - fakeInflearn",
+  description: "Fake inflearn(인프런) 로그인 페이지입니다.",
+};
 
 export default function SignInPage() {
   return <SignInUI />;
